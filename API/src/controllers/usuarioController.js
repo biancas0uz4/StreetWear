5edf66675e9c8d8e4b9ed4b1efe7e823435d15d7
biancas0uz4 <bianca.rsouza@sptech.school>
2024-05-18 01:38:45 -0300

    Consegui alterar a api no cadastro

--- API/src/controllers/usuarioController.js
+++ API/src/controllers/usuarioController.js
@@ -54,26 +54,26 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
+    var sobrenome = req.body.sobrenomeServer;
+    var telefone = req.body.telefoneServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var empresaId = req.body.empresaServer;
-    var cpf = req.body.cpfServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    } else if (sobrenome == undefined) {
+        res.status(400).send("Sua empresa está undefined!");
+    } else if (telefone == undefined){
+        res.status(400).send("Seu CPF está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (empresaId == undefined) {
-        res.status(400).send("Sua empresa está undefined!");
-    } else if (cpf == undefined){
-        res.status(400).send("Seu CPF está undefined!");
     }else{
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, empresaId, cpf)
+        usuarioModel.cadastrar(nome, sobrenome, telefone, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
