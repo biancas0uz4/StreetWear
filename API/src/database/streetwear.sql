f12475334077eccf9b6232a87fe7f65a3181c2da
biancas0uz4 <bianca.rsouza@sptech.school>
2024-05-23 23:11:20 -0300

    adicionei novos selects

---
+++ API/src/database/streetwear.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE streetwear;
+USE streetwear;
+
+CREATE TABLE usuario (
+idUser int primary key auto_increment,
+nome varchar(45),
+sobrenome varchar(45),
+telefone char(12),
+email varchar(50),
+senha varchar(50));
+
+SELECT * FROM usuario;
+
+CREATE TABLE roupa (
+idRoupa int primary key auto_increment,
+nome varchar(45));
+
+SELECT * FROM roupa;
+
+CREATE TABLE votacao (
+idVotacao int auto_increment,
+fkUser int,
+fkRoupa int,
+primary key (idVotacao, fkUser, fkRoupa),
+foreign key (fkUser) 
+references usuario(idUser),
+foreign key (fkRoupa)
+references roupa(idRoupa),
+dataHora datetime);
+
+SELECT * FROM votacao;
+SELECT * FROM roupa;
+
+
+SELECT usuario.nome as Usuário,
+ usuario.email as Email, 
+ roupa.nome as 'Conjunto Escolhido',
+ votacao.dataHora as 'DataHora da Votação'
+ FROM votacao JOIN usuario
+ ON fkUser = idUser
+ JOIN roupa 
+ ON fkRoupa = idRoupa;
+ 
+ SELECT 
+ roupa.nome as 'Conjunto Escolhido',
+ count(idVotacao) as 'Conjunto Vencedor'
+ FROM votacao 
+ JOIN roupa 
+ ON fkRoupa = idRoupa
+ group by  roupa.nome;
+ 
+ SELECT * FROM votacao JOIN usuario
+ ON fkUser = idUser
+ LEFT JOIN roupa 
+ ON fkRoupa = idRoupa;
